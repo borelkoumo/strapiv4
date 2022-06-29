df46d3b9a254d4e8b6243cbd3751c1b30154bf2d
Borel KOUMO <borelkoumo@gmail.com>
2022-06-29 22:08:15 +0200

    Change permissions

--- app/src/seeders/enablePermissions.js
+++ app/src/seeders/enablePermissions.js
@@ -7,18 +7,8 @@
    * @param {*} action The action itself
    */
 
-const enablePermission = async (strapi, roleType, actionId) => {
-  // get the role entity
+const enablePermission = async (strapi, role, actionId) => {
   try {
-    const role = await strapi.db
-      .query("plugin::users-permissions.role")
-      .findOne({
-        where: {
-          type: roleType,
-        },
-        populate: ["permissions"],
-      });
-
     // Get permissions associated with the role
     const rolePermissions = role.permissions.find(
       (permission) => permission.action === actionId
@@ -41,8 +31,6 @@ const enablePermission = async (strapi, roleType, actionId) => {
     console.log(error);
   }
 }
-module.exports.enablePermission = enablePermission
-
 
 /**
  * Enable actions for different roles in our app
@@ -51,21 +39,107 @@ module.exports.enablePermission = enablePermission
 const enablePermissions = async (strapi, defaultPermissions) => {
   const result = []
 
-  for (const role of defaultPermissions) {
-    const { roleType, permissions } = role
+  for (const defaultPermission of defaultPermissions) {
+    const { roleType, permissions } = defaultPermission
+    const role = await strapi.db
+      .query("plugin::users-permissions.role")
+      .findOne({
+        where: {
+          type: roleType,
+        },
+        populate: ["permissions"],
+      });
     for (const permission of permissions) {
       const { apiName, actions } = permission
       for (const action of actions) {
         const actionId = `${apiName}.${action}`;
         let p = await enablePermission(
           strapi,
-          roleType,
+          role,
           actionId
         );
         result.push({ roleType, actionId, permission: p })
       }
     }
-    return result;
   }
+  return result;
 }
 module.exports.enablePermissions = enablePermissions
+
+
+
+
+// const enablePermission2 = (strapi, role, actionId) => {
+//   try {
+//     // Get permissions associated with the role
+//     const rolePermissions = role.permissions.find(
+//       (permission) => permission.action === actionId
+//     );
+//     if (!rolePermissions) {
+//       // permission not yet created
+//       return strapi.db
+//         .query("plugin::users-permissions.permission")
+//         .create({
+//           data: {
+//             action: actionId,
+//             role: role.id,
+//           },
+//         });
+//     }
+//   } catch (error) {
+//     strapi.log.error(
+//       `Bootstrap script: Could not update settings. ${error.message}`
+//     );
+//     console.log(error);
+//   }
+// }
+// module.exports.enablePermission2 = enablePermission2
+
+
+// /**
+//  * Enable actions for different roles in our app
+//  * @param {*} strapi Strapi object
+//  */
+// const enablePermissions2 = async (strapi, defaultPermissions) => {
+//   const result = []
+
+//   for (const role of defaultPermissions) {
+//     const { roleType, permissions } = role
+//     for (const permission of permissions) {
+//       const { apiName, actions } = permission
+//       for (const action of actions) {
+//         const actionId = `${apiName}.${action}`;
+//         let p = await enablePermission(
+//           strapi,
+//           roleType,
+//           actionId
+//         );
+//         result.push({ roleType, actionId, permission: p })
+//       }
+//     }
+//     return result;
+//   }
+
+//   const arr = []
+//   for (const defaultPermission of defaultPermissions) {
+//     const { roleType, permissions } = defaultPermission
+//     const role = await strapi.db
+//       .query("plugin::users-permissions.role")
+//       .findOne({
+//         where: {
+//           type: roleType,
+//         },
+//         populate: ["permissions"],
+//       });
+//     for (const permission of permissions) {
+//       const { apiName, actions } = permission
+//       for (const action of actions) {
+//         const actionId = `${apiName}.${action}`;
+//         arr.push(enablePermission())
+//         result.push({ roleType, actionId, permission: p })
+//       }
+//     }
+//     return result;
+//   }
+// }
+// module.exports.enablePermissions2 = enablePermissions2
